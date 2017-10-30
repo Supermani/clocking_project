d1c501e3a0691b8bdc7bda39fa46a6b0fcbb7f6a
zhangyingwen <zhangyw@primeton.com>
2017-10-30 14:45:00 +0800

    修改节假日加班天数导出统计

--- project_manage/src/main/java/com/primeton/manage/statistics/AttendanceCalculation.java
+++ project_manage/src/main/java/com/primeton/manage/statistics/AttendanceCalculation.java
@@ -38,19 +38,28 @@ public class AttendanceCalculation {
 		for (int i = 0; i < empList.size(); i++) {
 			EmployeeInfo emp = empList.get(i);
 			int monthlyAttendanceDay = attRepository.findAttendanceDays(emp.id, startDate, endDate); // 工作日出勤天数
-			int holidayWorkingDays = attRepository.findHolidayWorkingDays(emp.id, startDate, endDate);// 节假日加班天数
-			double halfDays = attRepository.findHalfDays(emp.id, startDate, endDate) / 2.0; // 节假日加班半天天数
 			double workOverTimes = attRepository.findWorkOverTime(emp.id, startDate, endDate);// 工作日加班小时数
 			double workOverTimeDays = workOverTimes / 8.0;// 工作日加班天数
+			/*
+			 * 节假日加班天数=半天天数 + 一天天数 + 小时数
+			 * 	加班小时数 < 4h == 0.5day 半天
+			 * 	4h<= 加班小时数 <= 8h   == 1day 一天
+			 *  加班小时数 > 8h  == 1 + (加班小时数-8)/8 day 一天+多出来的小时数
+			 */
+			int holidayWorkingDays = attRepository.findHolidayWorkingDays(emp.id, startDate, endDate);// 节假日加班天数(4-8h)
+			double halfDays = attRepository.findHalfDays(emp.id, startDate, endDate) / 2.0; // 节假日加班半天天数(<4h)
+			double holidayWorkingHourDays = attRepository.findHolidayWorkingHours(emp.id, startDate, endDate);// 节假日加班小时数（转换天）(>8h)
+			double allHolidayWorkings = holidayWorkingDays + halfDays + holidayWorkingHourDays / 8.0; // 节假日加班总天数
+
 			Map<String, Object> map = new HashMap<>();
 			map.put("Id", emp.id);
 			map.put("Name", emp.name);
 			map.put("MonthlyAttendanceDay", monthlyAttendanceDay);
-			map.put("HolidayWorkingDays", holidayWorkingDays + halfDays);
+			map.put("HolidayWorkingDays", allHolidayWorkings);
 			map.put("WorkOverTimes", workOverTimes);
 			map.put("WorkOverTimeDays", workOverTimeDays);
 			int workingDays = getWorkingDays(startDate, endDate);
-			double allDays = monthlyAttendanceDay + holidayWorkingDays + halfDays + workOverTimeDays;
+			double allDays = monthlyAttendanceDay + allHolidayWorkings + workOverTimeDays;
 			// 人月数=(工作日出勤天数+节假日出勤天数+工作日加班天数)/工作日天数
 			double monthlyRadio = 0.0;
 			if (workingDays != 0) {
